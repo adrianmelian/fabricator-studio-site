1d228cd4bd43323725a4c70ed8cfdcdc8de46e17
amelian <adrianmelian123@gmail.com>
2026-07-02 17:46:59 -0600

    [Site] NavBar: tabs, community dropdown, single filled-plasma download

---
+++ src/data/links.ts
@@ -0,0 +1,12 @@
+// src/data/links.ts
+// HONESTY RULE: only links that exist ship. Discord/X get added when created.
+export const LINKS = {
+  download: 'https://github.com/adrianmelian/KinematicSolutions/releases/latest',
+  tutorialsChannel: 'https://www.youtube.com/@age914',
+  wiki: 'https://github.com/adrianmelian/KinematicSolutions',
+  repo: 'https://github.com/adrianmelian/KinematicSolutions',
+  community: [
+    { label: 'GitHub', href: 'https://github.com/adrianmelian/KinematicSolutions' },
+    { label: 'Instagram', href: 'https://www.instagram.com/fabricator.studio' },
+  ],
+};
